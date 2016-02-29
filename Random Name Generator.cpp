8f355a862121507e214a1706d655e7ef8132e823
Jake Ralph <flooblegoo@gmail.com>
2016-02-29 21:47:33 +1100

    Random Name Generator
    Challenge #1 - Project Files

---
+++ Random Name Generator.cpp
@@ -0,0 +1,25 @@
+// Challenge #1 - Random Name Generator
+// By Jake R. using Visual Studio and my penis
+
+#include "stdafx.h"
+#include <string>
+#include <iostream>
+#include <ctime>
+
+int main()
+{
+	std::string fNames[6] = { "John", "Fred", "Tyson", "Snoop", "Larry", "Jacob" };
+	std::string lNames[6] = { "Jackson", "Dogg", "Frederikson", "Burns", "Haley", "Aturken" };
+	std::string addrNames[4] = { "123 Fake St, North Exampleton", "189 Not Real Rd, Ghostville", "94 Siegfried Cr, Townsville", "12 John Crt, Jackstown" };
+	std::string emailNames[4] = { "xbox1sg4y@example.com", "urmom4life@example.com", "omgwtfbbq@example.com", "siegfriedandroy@example.com" };
+	int phNumbers[6] = { 52794613, 57784456, 58786163, 57648797, 57412242, 55484545 };
+
+	std::string firstName;
+	std::string lastName;
+	std::string addressName;
+	std::string emailName;
+	int phoneNum;
+
+    return 0;
+}
+
