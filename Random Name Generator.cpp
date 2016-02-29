371fc1bbc739fa0b1ca32e210d85e5c38d5fbfaf
Jake Ralph <flooblegoo@gmail.com>
2016-02-29 22:48:43 +1100

    Version 1.0
    Still requires some editing of the RNG method as it generates the same
    thing too often due to it being based on system time. Program now cycles
    stable though.

--- Random Name Generator.cpp
+++ Random Name Generator.cpp
@@ -14,30 +14,33 @@ int main()
 	// Declaring each array and acquiring a seperate random number for each one because I am retarded and 
 	// can't figure a good way to just reroll the fucking thing whatever this'll do
 
+	// NOTE TO SELF - MAJOR DOWNSIDE USING SYS TIME FOR RNG IS THAT RESULT IS OFTEN VERY SIMILAR
+	// THE NAMES, FOR EXAMPLE, WILL ALMOST ALWAYS BE THE SAME
+
 	std::string fNamesArray[6] = { "John", "Fred", "Tyson", "Snoop", "Larry", "Jacob" };
 	srand(time(0));
 	int fNamesRand = rand();
-	int fNamesNumb = (fNamesRand % 6) + 1;
+	int fNamesNumb = (fNamesRand % 6);
 
 	std::string lNamesArray[6] = { "Jackson", "Dogg", "Frederikson", "Burns", "Haley", "Aturken" };
 	srand(time(0));
 	int lNamesRand = rand();
-	int lNamesNumb = (lNamesRand % 6) + 1;
+	int lNamesNumb = (lNamesRand % 6);
 
 	std::string addrNamesArray[4] = { "123 Fake St, North Exampleton", "189 Not Real Rd, Ghostville", "94 Siegfried Cr, Townsville", "12 John Crt, Jackstown" };
 	srand(time(0));
 	int addrNamesRand = rand();
-	int addrNamesNumb = (addrNamesRand % 4) + 1;
+	int addrNamesNumb = (addrNamesRand % 4);
 
 	std::string emailNamesArray[4] = { "xbox1sg4y@example.com", "urmom4life@example.com", "omgwtfbbq@example.com", "siegfriedandroy@example.com" };
 	srand(time(0));
 	int emailNamesRand = rand();
-	int emailNamesNumb = (emailNamesRand % 4) + 1;
+	int emailNamesNumb = (emailNamesRand % 4);
 
 	int phNumbersArray[6] = { 52794613, 57784456, 58786163, 57648797, 57412242, 55484545 };
 	srand(time(0));
 	int phNumbRand = rand();
-	int phNumbNumb = (phNumbRand % 6) + 1;
+	int phNumbNumb = (phNumbRand % 6);
 
 	std::string firstName = fNamesArray[fNamesNumb];
 	std::string lastName = lNamesArray[lNamesNumb];
@@ -54,35 +57,39 @@ int main()
 	std::cout << "EMAIL: " << emailName << "\n";
 	std::cout << "PHONE: " << phoneNum << "\n";
 
-	// Asking user if they wish to generate more details
-
-	std::cout << "\nWould you like to generate another name? Y/N: ";
-	std::cin >> restartGenerator;
-	std::cin.ignore();
+	
 
 	// Checking user response to determine whether or not to restart
 	while (true)
 	{
+		// Asking user if they wish to generate more details
+
+		std::cout << "\nWould you like to generate another name? Y/N: ";
+		std::cin >> restartGenerator;
+		std::cin.ignore();
+
 		if (restartGenerator == "y" || restartGenerator == "Y" || restartGenerator == "n" || restartGenerator == "N")
 		{
 			if (restartGenerator == "Y" || restartGenerator == "y")
 			{
+				std::cout << "\n\n";
 				main();
 			}
 			if (restartGenerator == "N" || restartGenerator == "n")
 			{
 				std::cout << "\nThank-you!\n";
+				return false;
 			}
-		}
-		else
-		{
-			std::cout << "\nPlease enter Y/N!\n";
+			return false;
 		}
 
-		break;
+		else
+		{
+			std::cout << "\nPlease enter Y/N!\n\n";
+		}
+
 	}
 	
 
     return 0;
 }
-
