34a78c90669354a2f1f71a953cb5b02121da2aa5
Jake Ralph <flooblegoo@gmail.com>
2016-02-29 22:11:32 +1100

    Updated files with latest code

--- Random Name Generator.cpp
+++ Random Name Generator.cpp
@@ -8,17 +8,60 @@
 
 int main()
 {
-	std::string fNames[6] = { "John", "Fred", "Tyson", "Snoop", "Larry", "Jacob" };
-	std::string lNames[6] = { "Jackson", "Dogg", "Frederikson", "Burns", "Haley", "Aturken" };
-	std::string addrNames[4] = { "123 Fake St, North Exampleton", "189 Not Real Rd, Ghostville", "94 Siegfried Cr, Townsville", "12 John Crt, Jackstown" };
-	std::string emailNames[4] = { "xbox1sg4y@example.com", "urmom4life@example.com", "omgwtfbbq@example.com", "siegfriedandroy@example.com" };
-	int phNumbers[6] = { 52794613, 57784456, 58786163, 57648797, 57412242, 55484545 };
+	// Declaring restart y/n
+	std::string restartGenerator;
 
-	std::string firstName;
-	std::string lastName;
-	std::string addressName;
-	std::string emailName;
-	int phoneNum;
+	// Declaring each array and acquiring a seperate random number for each one because I am retarded and 
+	// can't figure a good way to just reroll the fucking thing whatever this'll do
+
+	std::string fNamesArray[6] = { "John", "Fred", "Tyson", "Snoop", "Larry", "Jacob" };
+	srand(time(0));
+	int fNamesRand = rand();
+	int fNamesNumb = (fNamesRand % 6) + 1;
+
+	std::string lNamesArray[6] = { "Jackson", "Dogg", "Frederikson", "Burns", "Haley", "Aturken" };
+	srand(time(0));
+	int lNamesRand = rand();
+	int lNamesNumb = (lNamesRand % 6) + 1;
+
+	std::string addrNamesArray[4] = { "123 Fake St, North Exampleton", "189 Not Real Rd, Ghostville", "94 Siegfried Cr, Townsville", "12 John Crt, Jackstown" };
+	srand(time(0));
+	int addrNamesRand = rand();
+	int addrNamesNumb = (addrNamesRand % 4) + 1;
+
+	std::string emailNamesArray[4] = { "xbox1sg4y@example.com", "urmom4life@example.com", "omgwtfbbq@example.com", "siegfriedandroy@example.com" };
+	srand(time(0));
+	int emailNamesRand = rand();
+	int emailNamesNumb = (emailNamesRand % 4) + 1;
+
+	int phNumbersArray[6] = { 52794613, 57784456, 58786163, 57648797, 57412242, 55484545 };
+	srand(time(0));
+	int phNumbRand = rand();
+	int phNumbNumb = (phNumbRand % 6) + 1;
+
+	std::string firstName = fNamesArray[fNamesNumb];
+	std::string lastName = lNamesArray[lNamesNumb];
+	std::string addressName = addrNamesArray[addrNamesNumb];
+	std::string emailName = emailNamesArray[emailNamesNumb];
+	int phoneNum = phNumbersArray[phNumbNumb];
+
+	// Outputting details generated
+
+	std::cout << "Welcome to the Random Name Generator\n\n";
+	std::cout << "Your generated details are as follows -\n";
+	std::cout << "NAME: " << firstName << " " << lastName << "\n";
+	std::cout << "ADDRESS: " << addressName << "\n";
+	std::cout << "EMAIL: " << emailName << "\n";
+	std::cout << "PHONE: " << phoneNum << "\n";
+
+	// Asking user if they wish to generate more details
+
+	std::cout << "\nWould you like to generate another name? Y/N: ";
+	std::cin >> restartGenerator;
+	std::cin.ignore();
+
+	// Checking user response to determine whether or not to restart
+	if()
 
     return 0;
 }
