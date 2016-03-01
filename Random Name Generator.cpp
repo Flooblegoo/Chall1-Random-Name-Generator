9859f703140df59787c21acacc8667246d40ab38
Jake Ralph <flooblegoo@gmail.com>
2016-03-01 13:56:46 +1100

    Version 2.0
    Next revision, Will fixed the Sys time problem by having it only be
    seeded (or w/e) once then just calling a random number from it each
    time. GG will you scrub

--- Random Name Generator.cpp
+++ Random Name Generator.cpp
@@ -14,31 +14,27 @@ int main()
 	// Declaring each array and acquiring a seperate random number for each one because I am retarded and 
 	// can't figure a good way to just reroll the fucking thing whatever this'll do
 
-	// NOTE TO SELF - MAJOR DOWNSIDE USING SYS TIME FOR RNG IS THAT RESULT IS OFTEN VERY SIMILAR
-	// THE NAMES, FOR EXAMPLE, WILL ALMOST ALWAYS BE THE SAME
+	// SYS TIME RAND FIXED BY WILL - ONLY CALL SRAND ONCE BELOW THEN JUST USE RAND()
+	// Seeding rand() or w/e the fuck you say, anyway that's the seed call here
+	srand(time(0));
 
 	std::string fNamesArray[6] = { "John", "Fred", "Tyson", "Snoop", "Larry", "Jacob" };
-	srand(time(0));
 	int fNamesRand = rand();
 	int fNamesNumb = (fNamesRand % 6);
 
 	std::string lNamesArray[6] = { "Jackson", "Dogg", "Frederikson", "Burns", "Haley", "Aturken" };
-	srand(time(0));
 	int lNamesRand = rand();
 	int lNamesNumb = (lNamesRand % 6);
 
 	std::string addrNamesArray[4] = { "123 Fake St, North Exampleton", "189 Not Real Rd, Ghostville", "94 Siegfried Cr, Townsville", "12 John Crt, Jackstown" };
-	srand(time(0));
 	int addrNamesRand = rand();
 	int addrNamesNumb = (addrNamesRand % 4);
 
 	std::string emailNamesArray[4] = { "xbox1sg4y@example.com", "urmom4life@example.com", "omgwtfbbq@example.com", "siegfriedandroy@example.com" };
-	srand(time(0));
 	int emailNamesRand = rand();
 	int emailNamesNumb = (emailNamesRand % 4);
 
 	int phNumbersArray[6] = { 52794613, 57784456, 58786163, 57648797, 57412242, 55484545 };
-	srand(time(0));
 	int phNumbRand = rand();
 	int phNumbNumb = (phNumbRand % 6);
 
